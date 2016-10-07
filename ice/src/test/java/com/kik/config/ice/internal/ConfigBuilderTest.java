15f856e51540190c31a85483a93ab1881c4bcf08
Dan Hendry <dan.hendry@kik.com>
2016-10-07 16:01:17 -0400

    Another type of test

--- ice/src/test/java/com/kik/config/ice/internal/ConfigBuilderTest.java
+++ ice/src/test/java/com/kik/config/ice/internal/ConfigBuilderTest.java
@@ -7,6 +7,7 @@ import com.kik.config.ice.ConfigSystem;
 import com.kik.config.ice.annotations.DefaultValue;
 import java.lang.ref.WeakReference;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -30,7 +31,7 @@ public class ConfigBuilderTest
      * leak.
      */
     @Test(timeout = 10_000)
-    public void testNoRefsToInjector() throws Exception
+    public void testNoStaticRefsToInjector() throws Exception
     {
         Injector injector = Guice.createInjector(
             ConfigConfigurator.testModules(),
@@ -39,9 +40,46 @@ public class ConfigBuilderTest
         assertEquals(123, injector.getInstance(Config.class).myValue());
 
         WeakReference<Injector> injectorRef = new WeakReference<>(injector);
-        injector = null; // Just to be specific
+        injector = null; // Just to be sure
 
-        for (int i = 0; i < 50 && injectorRef.get() != null; i++) {
+        // Calling System.gc() does not guarantee it gets run - try a bunch of times
+        for (int i = 0; i < 10 && injectorRef.get() != null; i++) {
+            System.gc();
+            Thread.sleep(10);
+            System.runFinalization();
+            System.gc();
+        }
+
+        assertNull(injectorRef.get());
+    }
+
+    @Test(timeout = 10_000)
+    public void testRefToInjectorWithClass() throws Exception
+    {
+        Injector injector = Guice.createInjector(
+            ConfigConfigurator.testModules(),
+            ConfigSystem.configModule(Config.class));
+
+        Config config = injector.getInstance(Config.class);
+        assertEquals(123, config.myValue());
+
+        WeakReference<Injector> injectorRef = new WeakReference<>(injector);
+        injector = null; // Just to be sure
+
+        System.gc();
+        Thread.sleep(50);
+        System.runFinalization();
+        System.gc();
+
+        assertNotNull(injectorRef.get());
+
+        // Keep the config ref around
+        assertEquals(123, config.myValue());
+
+        WeakReference<Config> configRef = new WeakReference<>(config);
+        config = null;
+
+        for (int i = 0; i < 10 && (injectorRef.get() != null || configRef.get() != null); i++) {
 
             System.gc();
             Thread.sleep(50);
@@ -50,5 +88,6 @@ public class ConfigBuilderTest
         }
 
         assertNull(injectorRef.get());
+        assertNull(configRef.get());
     }
 }
